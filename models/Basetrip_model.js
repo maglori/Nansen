fe87fdf24836b2c5afcbcf2a7547b2a3cb91a0a9
Vincent Maglori <VM@Vincents-MBP.home>
2017-05-09 00:33:13 -0400

    .

---
+++ models/Basetrip_model.js
@@ -0,0 +1,27 @@
+var request = require('request');
+
+function BasetripAPI() {
+    var country = "France";
+
+    var options = {
+    url: 'https://thebasetrip.p.mashape.com/v2/countries/' + country,
+    headers: {
+        'X-Mashape-Key': 'wk0vIHbNHBmshGyNWgmNn4WNcfj9p1ORF9PjsnHUfmkkpWmPMi',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    };
+    
+    function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        console.log(info);
+    }
+    }
+    
+    request(options, callback);
+}
+
+// BasetripAPI();
+
+module.exports = BasetripAPI;
